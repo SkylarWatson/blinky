138b4c23eb1bd450bc49dc9ef14ce1515ab83760
Skylar <skylarrwatson@gmail.com>
2017-07-25 22:07:12 -0400

    Moved initalization of AnnotationLoader into setup method

--- src/test/java/com/watson/blink/api/AnnotationLoaderTest.java
+++ src/test/java/com/watson/blink/api/AnnotationLoaderTest.java
@@ -1,5 +1,6 @@
 package com.watson.blink.api;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
@@ -7,16 +8,23 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class AnnotationLoaderTest {
+    private AnnotationLoader loader;
+
+    @Before
+    public void setup() {
+        loader = new AnnotationLoader();
+    }
+
     @Test
     public void returnsSizeOfZeroWhenNoClassesToLoad() {
-        List<Class> classes = new AnnotationLoader().load(this, TestAnnotation.class);
+        List<Class> classes = loader.load(this, TestAnnotation.class);
 
         assertEquals(0, classes.size());
     }
 
     @Test
     public void foo() {
-        List<Class> classes = new AnnotationLoader().load(this, Blinker.class);
+        List<Class> classes = loader.load(this, Blinker.class);
 
         assertEquals("TestBlinker", classes.get(0).getSimpleName());
     }
