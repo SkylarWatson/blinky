5a77fc976a0e47a48dbf29f26d651748e955bba6
Skylar <skylarrwatson@gmail.com>
2017-07-25 22:11:28 -0400

    Inline local variables in AnnotationLoaderTest

--- src/test/java/com/watson/blink/api/AnnotationLoaderTest.java
+++ src/test/java/com/watson/blink/api/AnnotationLoaderTest.java
@@ -3,8 +3,6 @@ package com.watson.blink.api;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AnnotationLoaderTest {
@@ -17,16 +15,12 @@ public class AnnotationLoaderTest {
 
     @Test
     public void returnsSizeOfZeroWhenNoClassesToLoad() {
-        List<Class> classes = loader.load(this, TestAnnotation.class);
-
-        assertThat(classes).isEmpty();
+        assertThat(loader.load(this, TestAnnotation.class)).isEmpty();
     }
 
     @Test
-    public void foo() {
-        List<Class> classes = loader.load(this, Blinker.class);
-
-        assertThat(classes.get(0).getSimpleName()).isEqualTo("TestBlinker");
+    public void returnClassNameAnnotatedWithBlinkerAnnotation() {
+        assertThat(loader.load(this, Blinker.class).get(0).getSimpleName()).isEqualTo("TestBlinker");
     }
 
     public @interface TestAnnotation {
