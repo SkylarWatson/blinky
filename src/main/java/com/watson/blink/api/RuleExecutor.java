502c937105586d8458d692d65557a230fc8ab915
Skylar <skylarrwatson@gmail.com>
2017-07-26 19:35:13 -0400

    Return all classes from RuleExecutor.execute(...) that meet criteria in shouldRun(...) implemented methods

--- src/main/java/com/watson/blink/api/RuleExecutor.java
+++ src/main/java/com/watson/blink/api/RuleExecutor.java
@@ -1,21 +1,18 @@
 package com.watson.blink.api;
 
-import java.lang.reflect.Method;
-import java.util.Collections;
 import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 public class RuleExecutor {
     public <T extends BlinkRule> List<Class<? extends BlinkRule>> execute(List<Class<? extends BlinkRule>> classes) {
-        try {
-            if (!classes.isEmpty()) {
-                Method method = classes.get(0).getMethod("shouldRun");
-                Boolean result = (Boolean) method.invoke(classes.get(0).newInstance());
-
-                if (result) return Collections.singletonList(classes.get(0));
+        return classes.stream().filter(klass -> {
+            try {
+                return (Boolean) klass.getMethod("shouldRun").invoke(klass.newInstance());
+            } catch (Exception ignore) {
+                ignore.printStackTrace();
             }
-        } catch (Exception ignore) {
-            ignore.printStackTrace();
-        }
-        return Collections.emptyList();
+            return false;
+        }).collect(toList());
     }
 }
