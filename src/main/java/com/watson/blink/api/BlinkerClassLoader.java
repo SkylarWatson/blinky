9bed6f9224b18f769b9aa042461a8d5c5ac2b220
Skylar <skylarrwatson@gmail.com>
2017-07-25 22:03:34 -0400

    BlinkerClassLoader returns all classes that have a specific annotation

--- src/main/java/com/watson/blink/api/BlinkerClassLoader.java
+++ src/main/java/com/watson/blink/api/BlinkerClassLoader.java
@@ -1,10 +1,18 @@
 package com.watson.blink.api;
 
-import java.util.Collections;
+import org.reflections.Reflections;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BlinkerClassLoader {
-    public List<Class> load() {
-        return Collections.emptyList();
+    public <T> List<Class> load(T t, Class<? extends Annotation> annotation) {
+        ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+        configurationBuilder.addUrls(ClasspathHelper.forClass(t.getClass()));
+
+        return new ArrayList<>(new Reflections(configurationBuilder).getTypesAnnotatedWith(annotation));
     }
 }
--- src/test/java/com/watson/blink/api/BlinkerClassLoaderTest.java
+++ src/test/java/com/watson/blink/api/BlinkerClassLoaderTest.java
@@ -9,8 +9,24 @@ import static org.junit.Assert.*;
 public class BlinkerClassLoaderTest {
     @Test
     public void returnsSizeOfZeroWhenNoClassesToLoad() {
-        List<Class> classes = new BlinkerClassLoader().load();
+        List<Class> classes = new BlinkerClassLoader().load(this, TestAnnotation.class);
 
         assertEquals(0, classes.size());
+    }
+
+    @Test
+    public void foo() {
+        List<Class> classes = new BlinkerClassLoader().load(this, Blinker.class);
+
+        assertEquals("TestBlinker", classes.get(0).getSimpleName());
+    }
+
+    public @interface TestAnnotation {
+
+    }
+
+    @Blinker
+    public class TestBlinker {
+
     }
 }
