81f1ae3844ea5c9ee113c91bfc6201a9631d870a
Skylar <skylarrwatson@gmail.com>
2018-03-03 23:34:04 -0600

    Added a demo for example usage and manual testing

--- src/main/java/com/watson/blink/Main.java
+++
@@ -1,9 +0,0 @@
-package com.watson.blink;
-
-import com.watson.blink.internal.command.CommandSender;
-
-public class Main {
-    public static void main(String[] args) {
-        new CommandSender().send();
-    }
-}
---
+++ src/main/java/com/watson/blink/demo/BlinkDemo.java
@@ -0,0 +1,12 @@
+package com.watson.blink.demo;
+
+import com.watson.blink.api.BlinkRule;
+import com.watson.blink.api.Blinker;
+
+@Blinker
+public class BlinkDemo implements BlinkRule {
+    @Override
+    public boolean shouldRun() {
+        return true;
+    }
+}
