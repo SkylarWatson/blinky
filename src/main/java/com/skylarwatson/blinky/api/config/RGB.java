053efde70d3f944ae3175a1b0e092e8225820258
Skylar <skylarrwatson@gmail.com>
2018-03-12 20:33:57 -0500

    Added additional colors

--- src/main/java/com/skylarwatson/blinky/api/config/RGB.java
+++ src/main/java/com/skylarwatson/blinky/api/config/RGB.java
@@ -2,7 +2,11 @@ package com.skylarwatson.blinky.api.config;
 
 public enum RGB {
     RED((byte) 155, (byte) 0, (byte) 0),
-    GREEN((byte) 0, (byte) 128, (byte) 0);
+    GREEN((byte) 0, (byte) 128, (byte) 0),
+    BLUE((byte) 0, (byte) 0, (byte) 255),
+    WHITE((byte) 255, (byte) 255, (byte) 255),
+    YELLOW((byte) 255, (byte) 255, (byte) 0),
+    OFF((byte) 0, (byte) 0, (byte) 0);
 
     byte r, b, g;
 
