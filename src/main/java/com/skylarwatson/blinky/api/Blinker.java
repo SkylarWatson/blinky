7929ea550ef2053e81560427e075fd0115a5940e
Skylar <skylarrwatson@gmail.com>
2018-03-12 20:23:04 -0500

    Set default color to red

--- src/main/java/com/skylarwatson/blinky/api/Blinker.java
+++ src/main/java/com/skylarwatson/blinky/api/Blinker.java
@@ -8,5 +8,5 @@ import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Blinker {
-    RGB color() default RGB.GREEN;
+    RGB color() default RGB.RED;
 }
