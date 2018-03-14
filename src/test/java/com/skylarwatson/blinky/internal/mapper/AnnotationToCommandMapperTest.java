00c21496fcda64806e96dbbd7310a1e68a8a1c1f
Skylar <skylarrwatson@gmail.com>
2018-03-14 18:10:18 -0500

    Moved initialization of AnnotationToCommandMapper to setup method

--- src/test/java/com/skylarwatson/blinky/internal/mapper/AnnotationToCommandMapperTest.java
+++ src/test/java/com/skylarwatson/blinky/internal/mapper/AnnotationToCommandMapperTest.java
@@ -3,22 +3,30 @@ package com.skylarwatson.blinky.internal.mapper;
 import com.skylarwatson.blinky.api.config.LED;
 import com.skylarwatson.blinky.api.config.RGB;
 import com.skylarwatson.blinky.internal.TestBlinker;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AnnotationToCommandMapperTest {
+    private AnnotationToCommandMapper mapper;
+
+    @Before
+    public void setUp() {
+        mapper = new AnnotationToCommandMapper();
+    }
+
     @Test
     public void map_set_led_on_command() {
         TestBlinker blinker = new TestBlinker();
 
-        assertThat(new AnnotationToCommandMapper().map(blinker.getClass()).getLed()).isEqualTo(LED.TOP);
+        assertThat(mapper.map(blinker.getClass()).getLed()).isEqualTo(LED.TOP);
     }
 
     @Test
     public void map_set_color_on_command() {
         TestBlinker blinker = new TestBlinker();
 
-        assertThat(new AnnotationToCommandMapper().map(blinker.getClass()).getColor()).isEqualTo(RGB.GREEN);
+        assertThat(mapper.map(blinker.getClass()).getColor()).isEqualTo(RGB.GREEN);
     }
 }
